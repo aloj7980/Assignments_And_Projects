27cb28ab2744128868dcaa32fef1cfc9ebc3eab6
Alex Ojemann <82542046+aloj7980@users.noreply.github.com>
2023-05-26 14:53:21 -0600

    Add files via upload

---
+++ CSCI3308 Software Development Methods and Tools/Labs/lab_7/init_data/create.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE IF EXISTS names CASCADE;
+CREATE TABLE IF NOT EXISTS names(
+    fname VARCHAR(35)
+);
+INSERT INTO names(fname)
+VALUES ('Logan Park'),
+('Alex Ojemann'),
+('Zach Wrubel'),
+('Tim Wilson'),
+('Jonathan Noranbrock')
+;
